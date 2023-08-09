7da7f7a93ffa131fa51c2a3503cee0e9a863db31
manism <manish.m@oracle.com>
2023-08-09 20:26:05 +0530

    Modified CCA

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import DataGridDemo from './DataGrid';
 
 
 
 function App() {
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     async function postData(url = "", data = {}) {
@@ -22,16 +23,38 @@ function App() {
       return response.json();
     }
 
-    postData("https://medical-result-5df91-default-rtdb.firebaseio.com/visits.json", { timestamp: new Date() }).then((data) => {
+    async function getData(url = "") {
+      const response = await fetch(url, {
+        method: "GET",
+        // mode: "no-cors", 
+        // cache: "no-cache", 
+        // credentials: "same-origin", 
+        // headers: {
+        //   "Content-Type": "application/json",
+        // },
+        // redirect: "follow", 
+        // referrerPolicy: "no-referrer", 
+      });
+      return response.json();
+    }
+
+    postData("https://medical-result-5df91-default-rtdb.firebaseio.com/visits.json", { timestamp: new Date(), appCodeName : navigator.appCodeName, appName : navigator.appName, userAgent : navigator.userAgent }).then((data) => {
       console.log(data); 
     });
+
+    getData("https://medical-result-5df91-default-rtdb.firebaseio.com/visits.json").then((data) => {
+      // console.log(data); 
+      setCount(Object.keys(data).length);
+    });
+
+
   }, [])
   return (
     <div className="App">
-      <h1>NEET-PG Counselling Seats Allotment -2023 Round 1 | Result Filter Application</h1>
-      <h6>Click on the Column Name, Three Vertical Dots, Filter to filter the table data, Best Viewed on Big Screens</h6>
-      <DataGridDemo />
-      <h6>The Application is meant for fair use only. For Feedback/comments mail us at redsoiltechnologies7@gmail.com </h6>
+      <h1 style={{textAlign : 'center'}}>NEET-PG Counselling Seats Allotment -2023 Round 1 | Result Filter Application</h1>
+        <DataGridDemo />
+      <h6 style={{textAlign : 'center'}}>Managed by Sandeep and Manish, The Application is meant for fair use only. For Feedback/comments mail us at redsoiltechnologies7@gmail.com </h6>
+      <h6 style={{textAlign : 'center'}}>Visit Counter: {count}</h6>
     </div>
   );
 }
